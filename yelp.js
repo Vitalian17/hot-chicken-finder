97ab8507c9a14e0c3b7e1780528af3b4146061f0
Tony Sarni <sarni.tony@gmail.com>
2019-11-20 11:43:32 -0600

    yelp api attached and working on button press

---
+++ yelp.js
@@ -0,0 +1,35 @@
+var yelpKey = "UkaLHk-lT2l73VMIoJSF6Iu7lh2ZocrRtzJ2OOacNV7cSTTL5Yge7PN3p0dpcAr1IDd1v2VY7L22UnjJPsFTKRIrxflMcZxAEpRYEDwK-IdByTHo7ortF1YYimXVXXYx";
+var yelpURL = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search";
+
+
+
+$("#hot-button").on("click", function(event){
+    event.preventDefault();
+    $.ajax({
+        method: "GET",
+        url: yelpURL + "?location=Nashville&term=hot chicken",
+        cache: true,
+        crossDomain: true,
+        headers: {
+            Authorization: "Bearer " + yelpKey,
+            "Access-Control-Allow-Origin": "*",
+            "x-requested-with": "xmlhttprequest"
+        }
+    }).then(function(yelpResults){
+        console.log(yelpResults);
+        var fullResults = yelpResults.businesses;
+
+        for (let i = 0; i < fullResults.length; i++){
+            var resultDiv = $("<div>").addClass("result-container");
+            var resultTitle = $("<h2>").text(fullResults[i].name);
+            var resultAddress = $("<div>");
+            for (let j = 0; j < fullResults[i].location.display_address.length; j++) {
+                resultAddress.append("<p>" + fullResults[i].location.display_address[j] + "</p>");
+            }
+            var phoneNumber = $("<p>").text(fullResults[i].display_phone);
+            resultDiv.append(resultTitle, resultAddress, phoneNumber);
+            $("#results").append(resultDiv);
+
+        }
+    })
+})
