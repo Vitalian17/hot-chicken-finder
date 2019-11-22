66590e98bc98c6b6e26680c3b1d2659e8de61a01
Tony Sarni <sarni.tony@gmail.com>
2019-11-22 09:55:07 -0600

    updated name and made media query for chicken icons

--- yelp.js
+++ yelp.js
@@ -1,6 +1,12 @@
 var yelpKey = "UkaLHk-lT2l73VMIoJSF6Iu7lh2ZocrRtzJ2OOacNV7cSTTL5Yge7PN3p0dpcAr1IDd1v2VY7L22UnjJPsFTKRIrxflMcZxAEpRYEDwK-IdByTHo7ortF1YYimXVXXYx";
 var yelpURL = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search";
+var markerIcon;
 
+if (window.matchMedia("(max-width: 700px)").matches) {
+    markerIcon = "";
+} else {
+    markerIcon = "https://files.slack.com/files-pri/TNRF54CSU-FQUCYV6Q0/bluchi.gif";
+}
 
 var currentLocation;
 $("#hot-button").on("click", function (event) {
@@ -70,7 +76,7 @@ $("#hot-button").on("click", function (event) {
             var latLng = new google.maps.LatLng(coords.latitude, coords.longitude);
             var marker = new google.maps.Marker({
                 position: latLng,
-                icon: "https://files.slack.com/files-pri/TNRF54CSU-FQUCYV6Q0/bluchi.gif",
+                icon: markerIcon,
                 map: map,
                 title: fullResults[i].name
             })
